e5a36a4153145c5027e363c84ac47f56e2aeedc8
Roman Zhukov <r.zhukov@itransition.com>
2015-07-21 16:13:46 +0300

    full proj commited

--- app/app.js
+++ app/app.js
@@ -1,3 +1,26 @@
-/**
- * Created by r.zhukov on 7/21/2015.
- */
+var scrumApp = angular.module('scrumApp', ['ngRoute', 'ngResource']);
+scrumApp.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl:"app/templates/Menu.html",
+            controller:'MenuController'
+        })
+        .when('#/login',{
+            templateUrl:"app/templates/Login.html",
+            controller:'LoginController'
+        })
+        .otherwise({redirectTo:'/'})
+});
+scrumApp.controller('BaseController', function ($scope) {
+
+});
+scrumApp.controller('MenuController', function($scope){
+
+});
+scrumApp.controller('LoginController',function($scope){
+    $scope.logIn = function(){
+
+    };
+    $scope.username="";
+    $scope.password="";
+});
